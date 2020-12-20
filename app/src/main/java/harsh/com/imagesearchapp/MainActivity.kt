31db523f840a92d298f1441919edd58e52257ae3
hArsh <123321pudaleharshal@gmail.com>
2020-12-20 20:07:35 +0530

    Initial commit

---
+++ app/src/main/java/harsh/com/imagesearchapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package harsh.com.imagesearchapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.os.BuildCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
